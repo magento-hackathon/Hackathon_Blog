97ddcdd740f149ba36d36c8e3b141c4b6003b7b4
Vincent Hornikx <vincenthornikx@gmail.com>
2015-05-29 12:25:51 +0200

    created repository interface and blogpost interface in /Api

---
+++ app/code/Hackathon/Blog/Api/BlogpostRepositoryInterface.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Hackathon\Blog\Api;
+
+interface BlogpostRepositoryInterface {
+
+	/**
+	 * Create Blog post
+	 * @param \Hackathon\Blog\Api\Data\BlogpostInterface $blogpost
+	 * @return \Hackathon\Blog\Api\Data\BlogpostInterface
+	 */
+	public function save(\Hackathon\Blog\Api\Data\BlogpostInterface $blogpost);
+
+	/**
+	 * @param int $id
+	 *
+	 * @return \Hackathon\Blog\Api\Data\BlogpostInterface
+	 */
+	public function getById(int $id);
+
+	/**
+	 * @param srtring $slug
+	 *
+	 * @return \Hackathon\Blog\Api\Data\BlogpostInterface
+	 */
+	public function getBySlug(srtring $slug);
+
+	/**
+	 * @param int $id
+	 *
+	 * @return bool Will return true if deleted
+	 */
+	public function deleteById(int $id);
+
+	/**
+	 * @param \Magento\Framework\Api\SearchCriteriaInterface $searchCriteria
+	 *
+	 * @return \Hackathon\Blog\Api\BlogpostSearchResultsInterface
+	 */
+	public function getList(\Magento\Framework\Api\SearchCriteriaInterface $searchCriteria);
+}
---
+++ app/code/Hackathon/Blog/Api/Data/BlogpostInterface.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Hackathon\Blog\Api\Data;
+
+interface BlogpostInterface {
+
+	/**
+	 * @return int
+	 */
+	public function getId();
+
+	/**
+	 * @param int $id
+	 * @return $this
+	 */
+	public function setId(int $id);
+
+	/**
+	 * @return string
+	 */
+	public function getSlug();
+
+	/**
+	 * @param string $slug
+	 *
+	 * @return $this
+	 */
+	public function setSlug(string $slug);
+
+	/**
+	 * @return string The current status (publish | scheduled | draft | review)
+	 */
+	public function getStatus();
+
+	/**
+	 * @param string $status
+	 *
+	 * @return $this
+	 */
+	public function setStatus(string $status);
+
+	/**
+	 * @return string
+	 */
+	public function getTitle();
+
+	/**
+	 * @param string|string $title
+	 *
+	 * @return $this
+	 */
+	public function setTitle(string $title);
+
+	/**
+	 * @return string
+	 */
+	public function getContent();
+
+	/**
+	 * @param string $content
+	 *
+	 * @return $this
+	 */
+	public function setContent(string $content);
+
+	/**
+	 * @return string
+	 */
+	public function getPostDate();
+
+	/**
+	 * @param string $postdate
+	 *
+	 * @return $this
+	 */
+	public function setPostdate(string $postdate);
+
+	/**
+	 * @return string
+	 */
+	public function getKeywords();
+
+	/**
+	 * @param string $keywords
+	 *
+	 * @return $this
+	 */
+	public function setKeywords(string $keywords);
+
+	/**
+	 * @return string
+	 */
+	public function getExcerpt();
+
+	/**
+	 * @param string $excerpt
+	 *
+	 * @return $this
+	 */
+	public function setExcerpt(string $excerpt);
+}
